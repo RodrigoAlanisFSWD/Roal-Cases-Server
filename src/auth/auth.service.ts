4974bc6487999f9ac99e82eba93a70552a4bf4d4
RodrigoAlanisFSWD <rodrigoalanisfswd@gmail.com>
2022-11-09 18:01:28 -0600

    Fixes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -57,6 +57,8 @@ export class AuthService {
         try {
             const user = await this.userService.findUserById(userId);
 
+            console.log(user)
+
             if (!user) throw new ForbiddenException("Access Denied")
 
             const verify = await bcrypt.compare(rt, user.hashedRt);
